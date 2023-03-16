ab98581e088d1860d456c5317713f4e2e3497a8b
Tobias Mesquita <tobias.mesquita@gmail.com>
2023-03-16 18:08:40 -0300

    links

--- src/components/LanguageSelect.vue
+++ src/components/LanguageSelect.vue
@@ -5,8 +5,13 @@
     :label="t('language')"
     :options="options"
   >
-    <template v-for="(_, slot) in slots" :key="slot" v-slot:[slot]="scope">
+    <template v-for="(_, slot) in _slots" :key="slot" v-slot:[slot]="scope">
       <slot :name="slot" v-bind="scope" :key="slot" />
+    </template>
+    <template v-slot:option="scope">
+      <q-item v-bind="scope.itemProps" :to="getRoute(scope.opt.value)">
+        {{ scope.opt.label }}
+      </q-item>
     </template>
   </q-select>
 </template>
@@ -15,7 +20,7 @@
 import { useLocaleStore } from 'src/stores/locale';
 import { computed, useSlots, useAttrs } from 'vue';
 import { useI18n } from 'vue-i18n';
-import { useRoute, useRouter } from 'vue-router';
+import { useRoute } from 'vue-router';
 import type { QSelectProps, QSelectSlots } from 'quasar';
 import type { Locales } from 'src/boot/i18n';
 
@@ -27,12 +32,17 @@ export interface LanguageSelectProps
 }
 
 const { t, locale } = useI18n();
-const router = useRouter();
 const route = useRoute();
 
 const attrs = useAttrs();
 const localeStore = useLocaleStore();
 const slots = useSlots() as never as QSelectSlots;
+const _slots = computed(() => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { option, ..._slots } = slots;
+  return _slots;
+});
+
 const props = withDefaults(defineProps<LanguageSelectProps>(), {
   standout: true,
   emitValue: true,
@@ -48,19 +58,23 @@ const options = computed(() => [
   { value: 'pt-BR', label: t('portuguese') },
 ]);
 
+function getRoute(val: Locales) {
+  return {
+    name: route.name as string,
+    params: {
+      ...route.params,
+      lang: val,
+    },
+    query: route.query,
+    replace: true,
+  };
+}
+
 const modelValue = computed({
   get() {
     return (route.params.lang as Locales) || localeStore.detected;
   },
   async set(val: Locales) {
-    router.replace({
-      name: route.name as string,
-      params: {
-        ...route.params,
-        lang: val,
-      },
-      query: route.query,
-    });
     await localeStore.setLocale(val);
     locale.value = localeStore.locale;
   },
